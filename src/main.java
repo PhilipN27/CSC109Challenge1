ad2a1bf48a184fb9eca5ed4a75d34fcdb2ae54cc
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-04-03 18:33:12 -0400

    cart

--- src/main.java
+++ src/main.java
@@ -1,7 +1,15 @@
 public class main 
 {
+    public static void Cart() {
+        // this will list all of the names and prices of each item
+        System.out.println("The items are: ");
+        // Lists the items
+        // end list
+        System.out.println("Do you want to go back to the main menu or do you want to go to checkout?");
+        //
+    }
     public static void main(String[] args)
     {
         
-    }    
+    }
 }
